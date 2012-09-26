37fd551ff3deb68ae0d4308565e0aa3eaf2aa210
Lusy Kusuma Wardani <lusykususmawardani@yahoo.com>
2012-09-26 12:49:45 +0700

    sesi 01

---
+++ sesi1/my-app/src/main/java/com/artivisi/project/App.java
@@ -0,0 +1,22 @@
+package com.artivisi.project;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] xx )
+    {
+        JFrame fr = new JFrame();
+		fr.setTitle("Halo Swing");
+		JOptionPane.showMessageDialog(null, "Halo Dialog");
+		fr.setSize(200,200);
+		fr.setVisible(true);
+		JPanel pnl = new JPanel();
+		fr.add(pnl);
+    }
+}
